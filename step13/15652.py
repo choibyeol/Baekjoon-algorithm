3e99a3ecba873b4fae2173d7e9b474901eb5ab72
ChoiByeol <honey333888@naver.com>
2020-10-23 23:08:07 +0900

    N과 M(4)

---
+++ step13/15652.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+visited = [False] * N
+out = []
+
+def solve(depth, idx, N, M):
+    if depth == M:
+        print(' '.join(map(str, out)))
+        return
+    for i in range(idx, N):
+        out.append(i+1)
+        solve(depth+1, i, N, M)
+        out.pop()
+
+solve(0, 0, N, M)
