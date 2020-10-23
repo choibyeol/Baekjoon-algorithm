1013694071f6390b65b7ee3d79e4f1a5e0291e09
ChoiByeol <honey333888@naver.com>
2020-10-23 22:49:24 +0900

    N과 M(2)

---
+++ step13/15650.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+visited = [False] * N
+out = []
+
+def solve(depth, N, M):
+    if depth == M:
+        is_sorted = (sorted(out) == out)
+        if is_sorted:
+            print(' '.join(map(str, out)))
+        return
+    for i in range(len(visited)):
+        if not visited[i]:
+            visited[i] = True
+            out.append(i+1)
+            solve(depth+1, N, M)
+            visited[i] = False
+            out.pop()
+
+solve(0, N, M)
