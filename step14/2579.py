d52ad80df13d44ce09e679eb289e0d09cc6bf6a6
ChoiByeol <honey333888@naver.com>
2020-11-19 00:00:10 +0900

    계단 오르기 수정

--- step14/2579.py
+++ step14/2579.py
@@ -10,7 +10,7 @@ for i in range(N):
     arr[i] = int(input())
 
 dp[0] = arr[0]
-dp[1] = max(arr[0]+arr[1], arr[1])
+dp[1] = arr[0]+arr[1]
 dp[2] = max(arr[0]+arr[2], arr[1]+arr[2])
 
 for i in range(3, N):
