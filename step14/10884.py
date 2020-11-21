54d816d620217bb9e25873ff8161c5bcee58a8d2
ChoiByeol <honey333888@naver.com>
2020-11-21 19:45:40 +0900

    쉬운 계단 수

---
+++ step14/10884.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+stair = [[0 for _ in range(10)] for _ in range(101)]
+
+for i in range(1, 101):
+    for j in range(10):
+        if i == 1:
+            stair[i][j] = 1
+        else:
+            if 1 <= j <= 8:
+                stair[i][j] = stair[i-1][j-1] + stair[i-1][j+1]
+            elif j == 0:
+                stair[i][j] = stair[i-1][j+1]
+            elif j == 9:
+                stair[i][j] = stair[i-1][j-1]
+
+N = int(input())
+print(sum(stair[N][1:10]) % 1000000000)
