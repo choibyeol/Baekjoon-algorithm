e57c43035d6bfd91043a43d6f724aeaa6521ab77
ChoiByeol <honey333888@naver.com>
2020-11-26 23:53:12 +0900

    LCS

---
+++ step14/9251.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+m = list(input())
+n = list(input())
+
+m_len = len(m) - 1
+n_len = len(n) - 1
+dp = [[0] * (n_len + 1) for i in range(m_len + 1)]
+
+for i in range(m_len):
+    for j in range(n_len):
+        if m[i] == n[j]:
+            dp[i + 1][j + 1] = dp[i][j] + 1
+        else:
+            dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+            
+print(dp[m_len][n_len])
