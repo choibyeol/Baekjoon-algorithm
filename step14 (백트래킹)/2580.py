c49e8f44d9d404e556e4d7e0ae136e3d6c1e0bd3
ChoiByeol <honey333888@naver.com>
2021-05-14 21:12:26 +0900

    step 제목 추가

--- step14 (백트래킹)/2580.py
+++ step14 (백트래킹)/2580.py
@@ -1,20 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-# 가로 체크
 def horizontal(x, val):
     if val in sudoku[x]:
         return False
     return True
 
-# 세로 체크
 def vertical(y, val):
     for i in range(9):
         if val == sudoku[i][y]:
             return False
     return True
 
-# 3x3 체크
 def three(x, y, val):
     nx = x//3 * 3
     ny = y//3 * 3
@@ -25,7 +22,7 @@ def three(x, y, val):
     return True
 
 def DFS(index):
-    if index == len(zeros):
+    if index == len(zero):
         for row in sudoku:
             for n in row:
                 print(n, end=" ")
@@ -33,15 +30,14 @@ def DFS(index):
         sys.exit(0)
     else:
         for i in range(1, 10):
-            nx = zeros[index][0]
-            ny = zeros[index][1]
-
-            # 세로, 가로, 3x3에 내가 대체하려고 하는 숫자가 존재하는지 확인
+            nx = zero[index][0]
+            ny = zero[index][1]
+            
             if horizontal(nx, i) and vertical(ny, i) and three(nx, ny, i):
                 sudoku[nx][ny] = i
                 DFS(index+1)
                 sudoku[nx][ny] = 0
 
 sudoku = [list(map(int, input().split())) for _ in range(9)]
-zeros = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
+zero = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
 DFS(0)
